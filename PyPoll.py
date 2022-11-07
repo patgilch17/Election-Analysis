e84509557f79baf641439d70721aab2189b286b6
patgilch17 <patrickgilchrist@Patricks-MacBook-Air.local>
2022-11-06 21:06:50 -0700

    added another practice, pypoll, and analysis

---
+++ PyPoll.py
@@ -0,0 +1,46 @@
+import csv
+import os
+
+#assign a variable for the pash to the csv file.  Using os.path.join so that in case this is run on a different
+#operating system the file path isn't messed up.  mac uses / windows uses \ resources/election_results.csv wouldn't run on windows.
+filetoread = os.path.join('Resources','election_results.csv')
+#election_data = open(filetoread,'r')
+
+#election_data.close()
+
+#open election results and read the file
+with open(filetoread,'r') as election_data:
+    #To Do: read and analyze the data here.
+    file_reader = csv.reader(election_data)
+
+    #print the header row
+    headers = next(file_reader)
+    print(headers)
+    #print each row in the csv file
+    #for row in file_reader:
+    #    print(row)
+
+filetowrite = os.path.join('Analysis','election_analysis.txt')
+
+#open the text file to write in
+with open(filetowrite,'w') as election_analysis:
+
+    #write to the text file
+    election_analysis.write("Counties in the Election \n")
+    election_analysis.write("----------------------\n")
+    election_analysis.write("Arapahoe\nDenver\nJefferson")
+    #election_analysis.write("Denver, ")
+    #election_analysis.write("Jefferson")
+    #open(filetowrite,'w')
+
+
+
+
+# Data needed via retrevial
+#A) Total number of votes cast
+#B) A complete list of candidates who received votes
+#C) The percent of votes each candidate won
+#D) The total numver of votes for each candidate
+#E) The winner based on the popular vote
+
+#close the 
